338dacb3c5b08c373f4211e7ae447633e614f0fa
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-23 14:47:24 +0200

    s

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 import streamlit as st
 
+title = '<h1 style="color:#AA4A44	;text-align:center; font-size: 5rem">Min and Max</h1>'
+st.markdown(title, unsafe_allow_html=True)
 
-
-
-
-def main():
-
-  title = '<h1 style="color:#AA4A44	;text-align:center; font-size: 5rem">Min and Max</h1>'
-  st.markdown(title, unsafe_allow_html=True)
-
-  sub_heading = '<p style="color:#4861C9;text-align:center; font-size: 2rem">Please give me five numbers</p>'
-  st.markdown(sub_heading, unsafe_allow_html=True)
+sub_heading = '<p style="color:#4861C9;text-align:center; font-size: 2rem">Please give me five numbers</p>'
+st.markdown(sub_heading, unsafe_allow_html=True)
 
 #   st.subheader("Please insert five numbers: I'll tell you the bigger and smaller")
-  st.markdown(
+st.markdown(
         f"""
          <style>
          .stApp {{
@@ -26,7 +20,11 @@ def main():
         unsafe_allow_html=True
     )
 
-st.slider(label='Choose a number', min_value=0, max_value=100, help='slide to choose', label_visibility='visible')
+
+
+
+def main():
+    st.slider(label='Choose a number', min_value=0, max_value=100, help='slide to choose', label_visibility='visible')
 
 
 
