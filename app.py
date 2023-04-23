813f56ca07fa9b55e45b86bce313d13bcdee8d9a
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-23 14:16:10 +0200

    s

--- app.py
+++ app.py
@@ -8,7 +8,18 @@ def main():
 
   st.title('Min & Max value')
   st.subheader("Pleas insert five numbers: I'll tell you the bigger and smaller")
-
+  st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background-image: url("https://shorturl.at/himn0");
+             background-attachment: fixed;
+             background-size: cover;
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
 
 
 
