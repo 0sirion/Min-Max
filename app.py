2522a784cb28446ad16d545fa79dedc38acd876d
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-23 14:28:50 +0200

    s

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ def main():
 
   title = '<h1 style="color:#AA4A44	;text-align:center; font-size: 5rem">Min and Max</h1>'
   st.markdown(title,unsafe_allow_html=True)
+  sub_heading = '<p style="color:#a77b1d;text-align:center; font-size: 5rem"></p>'
+  st.markdown(sub_heading,unsafe_allow_html=True)
 
-  st.subheader("Please insert five numbers: I'll tell you the bigger and smaller")
+#   st.subheader("Please insert five numbers: I'll tell you the bigger and smaller")
   st.markdown(
         f"""
          <style>
