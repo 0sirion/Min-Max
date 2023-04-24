ab8fe7ede9bd194dc530a931f16c32b6beb249e3
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-24 13:20:31 +0200

    s

--- app.py
+++ app.py
@@ -25,7 +25,12 @@ st.markdown(" :red[Please give me 3 numbers, I'll give you the smaller and bigge
 #     )
 
 def calculate_max(num1:float, num2:float, num3:float):
-     st.write(num1, num2, num3)
+     if num1 > num2 and num1 > num3:
+           return st.write("Highest number is: " + num1)
+     elif num2 > num1 and num2 > num3:
+          return st.write("Highest number is: " + num2)
+     elif num3 > num1 and num3 > num2:
+          return st.write("Highest number is: " + num3)
 
 
 def main():
