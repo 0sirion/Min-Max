582a50f4858fe82515eb14348a8a1bb864b0b92c
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-24 13:29:01 +0200

    s

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.markdown(" :red[Please give me 3 numbers, I'll give you the smaller and bigge
 
 def calculate_max(num1:float, num2:float, num3:float):
      if num1 > num2 and num1 > num3:
-           return st.write("Higqqqqqhest number is: ", num1)
+           return st.write("Highest number is: ", num1)
      elif num2 > num1 and num2 > num3:
           return st.write("Highest number is: ", num2)
      elif num3 > num1 and num3 > num2:
@@ -36,7 +36,7 @@ def calculate_max(num1:float, num2:float, num3:float):
 
 def calculate_min(num1:float, num2:float, num3:float):
      if num1 < num2 and num1 < num3:
-           return st.write("Miqqqqqnor number is: ", num1)
+           return st.write("Minor number is: ", num1)
      elif num2 < num1 and num2 < num3:
           return st.write("Minor number is: ", num2)
      elif num3 < num1 and num3 < num2:
@@ -51,7 +51,7 @@ def main():
 
      x3 = st.slider(label='Choose the third  number', min_value=0, max_value=100, help='slide to choose', label_visibility='visible')
 
-     st.write(calculate_max(x1,x2,x3))
-     st.write(calculate_min(x1,x2,x3))
+     calculate_max(x1,x2,x3)
+     calculate_min(x1,x2,x3)
 if __name__ == '__main__':
     main()
